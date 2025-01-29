39808c5bae785b8e9b76bc94da3760f6501c22fd
Hunter Dyar <bloop@hdyar.com>
2025-01-29 12:33:38 -0500

    Adders

---
+++ LilPuterSim/Components/Adder/Adder.cs
@@ -0,0 +1,53 @@
+﻿namespace LilPuter;
+
+public class Adder
+{
+	public Pin A;
+	public Pin B;
+	public Pin CarryIn;
+	public Pin SumOut;
+	public Pin CarryOut;
+
+	private XorGate _xorA;
+	private XorGate _xorB;
+	private AndGate _andA;
+	private AndGate _andB;
+	private OrGate _or;
+
+	private WireManager _manager;
+
+	public Adder(WireManager manager)
+	{
+		_manager = manager;
+		A = new Pin(manager, "AdderA");
+		B = new Pin(manager, "AdderB");
+		CarryIn = new Pin(manager, "AdderCarryIn");
+		SumOut = new Pin(manager, "AdderSumOut");
+		CarryOut = new Pin(manager, "AdderCarryOut");
+		_andA = new AndGate(manager);
+		_andB = new AndGate(manager);
+		_xorA = new XorGate(manager);
+		_xorB = new XorGate(manager);
+		_or = new OrGate(manager);
+
+		A.ConnectTo(_xorA.A);
+		A.ConnectTo(_andB.A);
+
+		B.ConnectTo(_xorA.B);
+		B.ConnectTo(_andB.B);
+
+		CarryIn.ConnectTo(_xorB.B);
+		CarryIn.ConnectTo(_andA.B);
+		
+		_xorA.Out.ConnectTo(_xorB.A);
+		_xorA.Out.ConnectTo(_andA.A);
+
+		_xorB.Out.ConnectTo(SumOut);
+		_andA.Out.ConnectTo(_or.A);
+		_andB.Out.ConnectTo(_or.B);
+		
+		_or.Out.ConnectTo(CarryOut);
+	}
+	
+	
+}
---
+++ LilPuterSimTest/AdderTest.cs
@@ -0,0 +1,118 @@
+﻿using LilPuter;
+
+namespace LilPuterSimTest;
+
+public class AdderTest
+{
+	private WireManager _manager;
+
+	[SetUp]
+	public void Setup()
+	{
+		_manager = new WireManager();
+	}
+
+
+	[Test]
+	public void HalfAdderLogicTableTests()
+	{
+		var ha = new HalfAdder(_manager);
+
+		_manager.SetPin(ha.A, WireSignal.Low);
+		_manager.SetPin(ha.B, WireSignal.Low);
+		Assert.That((WireSignal)ha.CarryOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(ha.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)ha.SumOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(ha.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(ha.B, WireSignal.High);
+		Assert.That((WireSignal)ha.CarryOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(ha.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)ha.SumOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(ha.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(ha.A, WireSignal.High);
+		_manager.SetPin(ha.B, WireSignal.Low);
+		Assert.That((WireSignal)ha.CarryOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(ha.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)ha.SumOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(ha.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(ha.A, WireSignal.High);
+		_manager.SetPin(ha.B, WireSignal.High);
+		Assert.That((WireSignal)ha.CarryOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(ha.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)ha.SumOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(ha.SumOut.Value.Length, Is.EqualTo(1));
+	}
+
+	[Test]
+	public void AdderLogicTableTests()
+	{
+		var a = new Adder(_manager);
+
+		_manager.SetPin(a.A, WireSignal.Low);
+		_manager.SetPin(a.B, WireSignal.Low);
+		_manager.SetPin(a.CarryIn, WireSignal.Low);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(a.A, WireSignal.Low);
+		_manager.SetPin(a.B, WireSignal.Low);
+		_manager.SetPin(a.CarryIn, WireSignal.High);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(a.A, WireSignal.Low);
+		_manager.SetPin(a.B, WireSignal.High);
+		_manager.SetPin(a.CarryIn, WireSignal.Low);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(a.A, WireSignal.Low);
+		_manager.SetPin(a.B, WireSignal.High);
+		_manager.SetPin(a.CarryIn, WireSignal.High);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(a.A, WireSignal.High);
+		_manager.SetPin(a.B, WireSignal.Low);
+		_manager.SetPin(a.CarryIn, WireSignal.Low);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(a.A, WireSignal.High);
+		_manager.SetPin(a.B, WireSignal.Low);
+		_manager.SetPin(a.CarryIn, WireSignal.High);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(a.A, WireSignal.High);
+		_manager.SetPin(a.B, WireSignal.High);
+		_manager.SetPin(a.CarryIn, WireSignal.Low);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.Low));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+
+		_manager.SetPin(a.A, WireSignal.High);
+		_manager.SetPin(a.B, WireSignal.High);
+		_manager.SetPin(a.CarryIn, WireSignal.High);
+		Assert.That((WireSignal)a.CarryOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.CarryOut.Value.Length, Is.EqualTo(1));
+		Assert.That((WireSignal)a.SumOut.Value[0], Is.EqualTo(WireSignal.High));
+		Assert.That(a.SumOut.Value.Length, Is.EqualTo(1));
+	}
+}
